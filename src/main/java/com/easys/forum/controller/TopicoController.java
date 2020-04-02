6376540a1e8f83ff8a89b9df0775313c402bbaa6
eric-anunciacao <anunciacao.eric@gmail.com>
2020-04-02 10:30:58 -0300

    Invalidando cache ao persistir os dados

--- src/main/java/com/easys/forum/controller/TopicoController.java
+++ src/main/java/com/easys/forum/controller/TopicoController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -57,6 +58,7 @@ public class TopicoController {
 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "topicosList", allEntries = true)
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -75,6 +77,7 @@ public class TopicoController {
 
 	@Transactional
 	@PutMapping("/{id}")
+	@CacheEvict(value = "topicosList", allEntries = true)
 	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -87,6 +90,7 @@ public class TopicoController {
 	@Transactional
 	@DeleteMapping("/{id}")
 	@SuppressWarnings("rawtypes")
+	@CacheEvict(value = "topicosList", allEntries = true)
 	public ResponseEntity remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
