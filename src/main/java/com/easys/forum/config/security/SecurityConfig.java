a20cea2551a65637a6222a338b138dcc727cddb5
eric-anunciacao <anunciacao.eric@gmail.com>
2020-04-02 14:04:58 -0300

    Implementando o monitoramento da API com Spring Boot Admin

--- src/main/java/com/easys/forum/config/security/SecurityConfig.java
+++ src/main/java/com/easys/forum/config/security/SecurityConfig.java
@@ -42,13 +42,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/auth").permitAll()				
-				.anyRequest().authenticated()
-				.and().csrf().disable()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().addFilterBefore(new TokenAuthenticationFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+			.anyRequest().authenticated()
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().addFilterBefore(new TokenAuthenticationFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Override
